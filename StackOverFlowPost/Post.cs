59cd7ab378b417f3f26ba02c48996dfc18e0f1a3
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-06-12 09:37:11 -0400

    initial commit

---
+++ StackOverFlowPost/Post.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace StackOverFlowPost
+{
+    public class Post
+    {
+        public string title;
+        public string description;
+        public DateTime PostDate { get; set; }
+        private int _votes { get; set; }
+
+        public int UpVote()
+        {
+            _votes++;
+            return _votes;
+        }
+
+        public int DownVote()
+        {
+            _votes--;
+            return _votes;
+        }
+
+        public Post()
+
+        {
+
+            Console.Write("What is the title of your post: ");
+            title = Console.ReadLine();
+            Console.Write("What is the body of your post: ");
+            description = Console.ReadLine();
+            PostDate = DateTime.Now;
+
+        }
+
+        public void DisplayPost()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Post Date: " + PostDate);
+            Console.WriteLine("Post Title: " + title);
+            Console.WriteLine("Post Body: " + description);
+            Console.WriteLine("Votes: " + _votes);
+        }
+
+    }
+}
+
---
+++ StackOverFlowPost/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StackOverFlowPost;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var FirstPost = new Post();
+        
+
+        FirstPost.UpVote();
+        FirstPost.UpVote();
+        FirstPost.UpVote();
+        FirstPost.DownVote();
+
+        FirstPost.DisplayPost();
+        Console.ReadLine();
+ 
+     }
+}
+
